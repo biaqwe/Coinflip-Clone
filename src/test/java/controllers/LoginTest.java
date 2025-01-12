4790f4ea8fd268ef0d81bb66049c402196a4e7a3
biaqwe <biancabalas2905@yahoo.com>
2025-01-12 23:10:57 +0200

    javadoc?

--- src/test/java/controllers/LoginTest.java
+++ src/test/java/controllers/LoginTest.java
@@ -4,15 +4,26 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.*;
-
+/**
+ * Clasa de test pentru functionalitatea de autentificare
+ */
 class LoginTest {
+	/**
+	 * Obiect de tip Login
+	 */
     private Login login;
     
+    /**
+     * Initializarea obiectului Login inainte de fiecare test
+     */
     @BeforeEach
     void setUp() {
         login=new Login();
     }
     
+    /**
+     * Testeaza scenariul in care numele de utilizator si parola sunt valide, verifica daca este returnat id ul corect pentru utilizator
+     */
     @Test
     void allValid() {
         String username="test";
@@ -21,6 +32,9 @@ class LoginTest {
         assertEquals(3, userID);
     }
 
+    /**
+     * Testeaza scenariul in care numele de utilizator nu e valid, verifica daca este returnat -1 pentru utilizator invalid
+     */
     @Test
     void invalidUsername() {
         String username="t";
@@ -29,6 +43,9 @@ class LoginTest {
         assertEquals(-1, userID);
     }
 
+    /**
+     * Testeaza scenariul in care parola nu e valida, verifica daca este returnat -1 pentru utilizator invalid
+     */
     @Test
     void invalidPassword() {
         String username="test";
@@ -37,6 +54,9 @@ class LoginTest {
         assertEquals(-1, userID);
     }
 
+    /**
+     * Testeaza scenariul in care numele de utilizator si parola nu sunt valide, verifica daca este returnat -1 pentru utilizator invalid
+     */
     @Test
     void allInvalid() {
         String username="miau";
